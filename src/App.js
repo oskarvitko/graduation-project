9f7a8d2950db3adb7dd03b9c53ea17559093f943
Oskar-Vitko1 <oskar.vitko1@ibm.com>
2022-01-27 15:37:05 +0300

    Reset project

--- src/App.js
+++ src/App.js
@@ -1,21 +1,7 @@
-import { CssBaseline, ThemeProvider } from '@material-ui/core'
-import { Provider } from 'react-redux'
-import { initInterceptors } from './http'
-import store from './redux'
-import AppRouter from './routing/index'
-import theme from './styles/theme'
-
-initInterceptors(store)
-
 function App() {
   return (
-    <Provider store={store}>
-      <CssBaseline>
-        <ThemeProvider theme={theme}>
-          <AppRouter />
-        </ThemeProvider>
-      </CssBaseline>
-    </Provider>
+    <>
+    </>
   )
 }
 
--- src/pages/MaterialsPage/index.js
+++
@@ -1,1 +0,0 @@
-export { default as MaterialsPage } from './Materials'
