674ce58933d709778de57db751ca3c3939488331
oskarvitko16@gmail.com <oskarvitko16@gmail.com>
2022-05-18 13:53:09 +0300

    update

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express')
 const favicon = require('express-favicon')
 const path = require('path')
 require('dotenv').config()
-const PORT = process.env.SERVER_PORT || 5001
+const PORT = process.env.PORT || 5001
 
 const app = express()
 
